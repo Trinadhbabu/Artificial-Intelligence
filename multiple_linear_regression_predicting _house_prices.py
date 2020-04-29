16c9b9b91f8fabaf019b67e124ef3819712350f8
Trinadh Singaladevi <trinadhbabu.s@gmail.com>
2020-04-29 21:34:06 +0530

    Update multiple_linear_regression_predicting _house_prices.py

--- multiple_linear_regression_predicting _house_prices.py
+++ multiple_linear_regression_predicting _house_prices.py
@@ -5,6 +5,20 @@ Created on Wed Apr 29 18:49:09 2020
 
 @author: Trinadh.Singaladevi
 """
+'''
+Objective
+In this challenge, we practice using multiple linear regression to predict housing prices. Check out the Resources tab for helpful videos!
+
+Task
+Charlie wants to buy a house. He does a detailed survey of the area where he wants to live, in which he quantifies, normalizes, and maps the desirable features of houses to values on a scale of  to  so the data can be assembled into a table. If Charlie noted  features, each row contains  space-separated values followed by the house price in dollars per square foot (making for a total of  columns). If Charlie makes observations about  houses, his observation table has  rows. This means that the table has a total of  entries.
+
+Unfortunately, he was only able to get the price per square foot for certain houses and thus needs your help estimating the prices of the rest! Given the feature and pricing data for a set of houses, help Charlie estimate the price per square foot of the houses for which he has compiled feature data but no pricing.
+
+Important Observation: The prices per square foot form an approximately linear function for the features quantified in Charlie's table. For the purposes of prediction, you need to figure out this linear function.
+
+Recommended Technique: Use a regression-based technique. At this point, you are not expected to account for bias and variance trade-offs.
+
+'''
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import sys
